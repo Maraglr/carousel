681f22384bc3a082c9653ab12fd2a74d81721a80
AguilarMariana <maraguilar012@gmail.com>
2024-05-31 11:19:57 -0400

    S

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-const carouselInner = document.querySelector('.carousel-inner');
-const items = document.querySelectorAll('.carousel-item');
+const carouselInner = document.getElementById('carouselInner');
+
+const images = [
+    'assets/img/img.webp',
+    'assets/img/img2.webp',
+    'assets/img/img3.webp',
+    'assets/img/img4.jpg',
+    'assets/img/img5.jpg',
+    'assets/img/img6.jpg',
+    'assets/img/img7.jpg'
+];
 
 let currentIndex = 0;
 
@@ -14,12 +23,17 @@ nextBtn.addEventListener('click', () => {
 });
 
 function showItem(index) {
-    if (index < 0) {
-        index = items.length - 1;
-    } else if (index >= items.length) {
-        index = 0;
-    }
-
-    carouselInner.style.transform = `translateX(-${index * 100}%)`;
-    currentIndex = index;
+    const totalImages = images.length;
+    currentIndex = (index + totalImages) % totalImages;
+    carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
+
+images.forEach((image, index) => {
+    const item = document.createElement('div');
+    item.classList.add('carousel-item');
+    const img = document.createElement('img');
+    img.src = image;
+    img.alt = `Image ${index + 1}`;
+    item.appendChild(img);
+    carouselInner.appendChild(item);
+});
